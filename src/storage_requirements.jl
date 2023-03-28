bdc2412049cc2a52a42c01b1530e85b4fec61c5b
wester <rolf.wester@mailbox.org>
2023-03-28 11:00:01 +0200

    seems to work

--- src/storage_requirements.jl
+++ src/storage_requirements.jl
@@ -12,14 +12,40 @@ get_fig_dir()    = joinpath(root, "figures")
 mkpath(get_fig_dir())
 
 
-eunit = ["MW", "GW", "TW"][2]
 
-plot_p     = false#true
-plot_all_p = false#true
-comp_and_plot(get_data_dir(), get_fig_dir(), eunit, plot_p=plot_p, plot_all_p=plot_all_p);
+function get_storage_capacities(punit)
+
+    stc2  = [14.0, 26.0, 35.0, 45.0]
+    oprod = [1.5, 1.2, 1.15, 1.1, 1.05]
+
+    #stc2  = [26.0]
+    #oprod = [1.2]
+
+    # stc1 < stc2
+    stc1 = @. stc2 * 0.1
+    stc2 = @. stc2 - stc1
+
+    conversion_factor = uconvert("TW", punit) * 0.6
+    stc1 = stc1 .* conversion_factor
+    stc2 = stc2 .* conversion_factor
+
+    stc1, stc2, oprod
+end
+
+punit = ["MW", "GW", "TW"][2]
+stc1, stc2, oprod = get_storage_capacities(punit)
+plot_p     = [false, true][2]
+plot_all_p = [false, true][2]
+do_log     = [false, true][1]
+comp_and_plot(stc1, stc2, oprod, get_data_dir(), get_fig_dir(), punit, plot_p=plot_p, plot_all_p=plot_all_p, do_log=do_log);
 
 # comp_and_plot_averaged(get_data_dir(), enunit, plot_p=true);
 7.01260e+04/(364*26)
 
 m = 252453600000.0
 m/(365*24*60*60*1.0e3)
+
+B = stg.IF[i] - stg.OF[i] - (stg.SF[i] - stg.SF[i-1])
+
+S =  2.60e+03
+ΔP = -1.00e+01, S =  2.59e+03, I =  0.00e+00, O = -1.00e+01, C =  0.00e+00, M =  0.00e+00, B =  2.01e+01
--- src/storage_requirements_functions.jl
+++ src/storage_requirements_functions.jl
@@ -15,6 +15,30 @@ get_fig_dir()    = joinpath(root, "figures")
 mkpath(get_fig_dir())
 
 """
+    dates
+    returns ΔTh - multiple of an hour per step
+"""
+function get_step_ΔTh(dates)
+    # Dates.value(DateTime) => ms since 1 AD
+    ms = Dates.value(dates[2] - dates[1])
+    # energy-chart data => 15 min time resolution ΔTh = 0.25
+    # energy-chart data averaged over 4 steps => 1h time resolution ΔTh = 1.0
+    # 1h = 60 * 60 * 1000 ms = 3.6e6 ms
+    ms/(3.6e6)
+end
+
+function nb_years(dates)
+    ΔT_ms_e  = Dates.value(dates[end] - dates[1])
+    Δh_e     = ΔT_ms_e/(3.6e6)
+    Δh_e/(365*24)
+end
+function powers_to_energy_per_year(dates, P)
+    ΔTh = get_step_ΔTh(dates)
+    nb_years = nb_years(dates)
+    sum(P)*ΔTh/nb_years
+end
+
+"""
     scale_and_detrend(Load, RP)
 
     renewable energy RP is scaled such that mean(RP) == mean(Load) over whole time considered (e.g. 2015 - 2022)
@@ -66,34 +90,43 @@ function Storage(SC::Float64, n::Int64)
     Storage(SC, SF, IF, OF, CT, IM)
 end
 
-function charge(st::Storage, ΔPin, i, out, j)
+function write_to_log(stg::Storage, ΔPin, out, i, j)
+    B = stg.IF[i] - stg.OF[i] - (stg.SF[i] - stg.SF[i-1])
+    write(out, @sprintf("%5d  %d  ΔP = %9.2e, S = %9.2e, I = %9.2e, O = %9.2e, C = %9.2e, M = %9.2e, B = %9.2e\n", i, j, ΔPin, stg.SF[i], stg.IF[i], stg.OF[i], stg.CT[i], stg.IM[i], B))
+end
+
+function charge(stg::Storage, ΔPin, i; out = nothing)
     local ΔPout
+    ΔSC = stg.SC - stg.SF[i-1]
     if ΔPin > 0.0
-        D = st.SC - st.SF[i-1]
-        if D > ΔPin
-            st.IF[i] = ΔPin
-            st.SF[i] = st.SF[i-1] + st.IF[i]
-            ΔPout    = 0.0
-            write(out, @sprintf("%d  1  ΔP=%9.2e, S=%9.2e, I=%9.2e, O=%9.2e, C=%9.2e, M=%9.2e\n", j, ΔPin, st.SF[i], st.IF[i], st.OF[i], st.CT[i], st.IM[i]))
+        if ΔSC > ΔPin
+            stg.IF[i] = ΔPin
+            stg.SF[i] = stg.SF[i-1] + stg.IF[i]
+            ΔPout    = 0.0     # Load is satisfied
+
+            if out !== nothing write_to_log(stg, ΔPin, out, i, 1) end
         else
-            st.IF[i] = D
-            st.SF[i] = st.SF[i-1] + st.IF[i]
-            st.CT[i] = ΔPin - st.IF[i]
-            ΔPout    = st.CT[i]
-            write(out, @sprintf("%d  2  ΔP=%9.2e, S=%9.2e, I=%9.2e, O=%9.2e, C=%9.2e, M=%9.2e\n", j, ΔPin, st.SF[i], st.IF[i], st.OF[i], st.CT[i], st.IM[i]))
+            stg.IF[i] = ΔSC
+            stg.SF[i] = stg.SF[i-1] + stg.IF[i]
+            stg.CT[i] = ΔPin - stg.IF[i] # to be curtailed (or exported)
+            ΔPout     = stg.CT[i] # Load is staisfied, ΔPout remains
+
+            if out !== nothing write_to_log(stg, ΔPin, out, i, 2) end
         end
     else
-        if st.SF[i-1] > -ΔPin
-            st.OF[i] = -ΔPin
-            st.SF[i] = st.SF[i-1] - st.OF[i]
-            ΔPout    = 0.0
-            write(out, @sprintf("%d  3  ΔP=%9.2e, S=%9.2e, I=%9.2e, O=%9.2e, C=%9.2e, M=%9.2e\n", j, ΔPin, st.SF[i], st.IF[i], st.OF[i], st.CT[i], st.IM[i]))
+        if stg.SF[i-1] > -ΔPin   # ΔPin < 0
+            stg.OF[i] = -ΔPin    # stg.OF[i] < 0
+            stg.SF[i] = stg.SF[i-1] - stg.OF[i]
+            ΔPout     = 0.0       # Load is satisfied
+
+            if out !== nothing write_to_log(stg, ΔPin, out, i, 3) end
         else
-            st.OF[i] = st.SF[i-1]
-            st.SF[i] = st.SF[i-1] - st.OF[i]
-            st.IM[i] = -ΔPin - st.OF[i]
-            ΔPout    = -st.IM[i]
-            write(out, @sprintf("%d  4  ΔP=%9.2e, S=%9.2e, I=%9.2e, O=%9.2e, C=%9.2e, M=%9.2e\n", j, ΔPin, st.SF[i], st.IF[i], st.OF[i], st.CT[i], st.IM[i]))
+            stg.OF[i] = stg.SF[i-1]
+            stg.SF[i] = stg.SF[i-1] - stg.OF[i]
+            stg.IM[i] = -ΔPin - stg.OF[i] # to be imported
+            ΔPout     = -stg.IM[i]        # missing for Load to be satisfied
+
+            if out !== nothing write_to_log(stg, ΔPin, out, i, 4) end
         end
     end
     ΔPout
@@ -105,7 +138,7 @@ end
 
     given Load, RP, over_production and storage_capacity compute storage level as a funtion of time
 
-    dates : times
+    dates : times, ΔT = 1h
     Load  : power consumed
     RP    : renewable power production
     punit : power unit of Load and RP (MW, GW, TW)
@@ -113,44 +146,46 @@ end
     storage_capacity : storage capacity
 """
 function compute_storage_level(dates::Vector{DateTime}, Load::Vector{Float64}, RP::Vector{Float64},
-    stc1::Float64, stc2::Float64, oprod::Float64)
-
-    # Dates.value(DateTime) => ms since 1 AD
-    ms = Dates.value(dates[2] - dates[1])
-    # energy-chart data => 15 min time resolution, 1h = 60 * 60 * 1000 ms = 3.6e6 ms
-    Δh = ms/(3.6e6)
-
-    # E => P * 15 min
-    stc1 = stc1 / Δh
-    stc2 = stc2 / Δh
+    stc1::Float64, stc2::Float64, oprod::Float64; do_log = false)
 
     # renewable energy over production
     RP = RP .* oprod
     # surplus or shortfall power production
     ΔP = (RP - Load)
 
-    n  = size(RP, 1)
-    out1 = open("llog1.log", "w")
-    out2 = open("llog2.log", "w")
+    out1, out2, out3 = nothing, nothing, nothing
+    if do_log
+        out1 = open(@sprintf("log_%3.1f_1.log", oprod), "w")
+        out2 = open(@sprintf("log_%3.1f_2.log", oprod), "w")
+        out3 = open(@sprintf("log_%3.1f_3.log", oprod), "w")
+    end
 
+    n  = size(RP, 1)
     stg1 = Storage(stc1, n)
     stg2 = Storage(stc2, n)
     for i in 2:n
-        ΔPout1 = charge(stg1, ΔP[i],  i, out1, 1)
-        ΔPout2 = charge(stg2, ΔPout1, i, out2, 2)
+        ΔPout1 = charge(stg1, ΔP[i],  i, out = out1)
+        ΔPout2 = charge(stg2, ΔPout1, i, out = out2)
+
+        balance1 = stg1.IF[i] - stg1.OF[i] - (stg1.SF[i] - stg1.SF[i-1])
+        balance2 = stg2.IF[i] - stg2.OF[i] - (stg2.SF[i] - stg2.SF[i-1])
+        if abs(balance1) > 1.0e-10 || abs(balance2) > 1.0e-10
+            write(out3, @sprintf("%d  b1 = %9.2e  b2 = %9.2e  P1 = %9.2e  P2 = %9.2e\n", i, balance1, balance2, ΔPout1, ΔPout2))
+        end
     end
-    E1I = energy(dates, stg1.IF)
-    E1O = energy(dates, stg1.OF)
-    E1F = stg1.SF[end]
-    E2I = energy(dates, stg2.IF)
-    E2O = energy(dates, stg2.OF)
-    E2F = stg2.SF[end]
 
-    @infoe @sprintf("Balance 1 = %9.3e, %9.3e, %9.3e, %9.3e", E1I, E1O, E1F,  E1I - E1O - E1F)
-    @infoe @sprintf("Balance 2 = %9.3e, %9.3e, %9.3e, %9.3e", E2I, E2O, E2F,  E2I - E2O - E2F)
+    @infoe @sprintf("op = %3.1f, SC1 = %8.2e, SC2 = %8.2e, SF1[end] = %8.2e, SF2[end] = %8.2e", oprod, stg1.SC, stg2.SC, stg1.SF[end], stg2.SF[end])
 
-    stg1.SF = stg1.SF .* Δh
-    stg2.SF = stg2.SF .* Δh
+    IF1 = sum(stg1.IF)
+    OF1 = sum(stg1.OF)
+    SF1 = stg1.SF[end]
+
+    IF2 = sum(stg2.IF)
+    OF2 = sum(stg2.OF)
+    SF2 = stg2.SF[end]
+
+    @infoe @sprintf("IF1 = %9.3e, OF1 = %9.3e, SF1[end] = %9.3e, B1 = %9.3e", IF1, OF1, SF1,  IF1 - OF1 - SF1)
+    @infoe @sprintf("IF2 = %9.3e, OF2 = %9.3e, SF2[end] = %9.3e, B2 = %9.3e", IF2, OF2, SF2,  IF2 - OF2 - SF2)
 
     (stg1, stg2)
 end
@@ -158,17 +193,13 @@ end
 """
     compute storage fill level for different combinations of storage_capacity and over_production
 """
-function compute_storage_fill_level(dates::Vector{DateTime}, Load::Vector{Float64}, RP::Vector{Float64}, punit::String, stc1, stc2, oprod)
-    @infoe @sprintf("stc1 = %s, stc2 = %s", stc1, stc2)
+function compute_storage_fill_level(dates::Vector{DateTime}, Load::Vector{Float64}, RP::Vector{Float64}, punit::String, stc1, stc2, oprod; do_log = false)
+    @infoe @sprintf("==== compute_storage_fill_level ===================================================================")
 
     res1 = []
     res2 = []
     for (sc1, sc2, op) in zip(stc1, stc2, oprod)
-        stg1, stg2 = compute_storage_level(dates, Load, RP, sc1, sc2, op)
-
-        @infoe @sprintf("sc1 = %8.2e, sc2 = %8.2e, op = %3.1f, SF1[end] = %8.2e, SC1 = %8.2e, SF2[end] = %8.2e, SC2 = %8.2e", 
-           sc1, sc2, op, stg1.SF[end], stg1.SC, stg2.SF[end], stg2.SC)
-
+        stg1, stg2 = compute_storage_level(dates, Load, RP, sc1, sc2, op, do_log = do_log)
         push!(res1, stg1)
         push!(res2, stg2)
     end
@@ -204,7 +235,7 @@ function determine_overproduction(dates::Vector{DateTime}, Load::Vector{Float64}
         end
         push!(storage_capacities, (stc1, stc2))
     end
-    plt.plot(overproduction, storage_capacities, "r.")
+    pl.plot(overproduction, storage_capacities, "r.")
 end
 
 """
@@ -321,53 +352,44 @@ function plot_storage_fill_level(dates::Vector{DateTime}, Load::Vector{Float64},
             pl.figure(fig[1]); fig[1] += 1
             pl.plot(dates, RP .* op ,   label="RP")
             pl.plot(dates, Load,   label="Load")
-            pl.title(@sprintf("%s Load, RP, %2.f %s, %3.f %s", title, sc, punit, (op-1.0)*100.0, prozent))
+            pl.xlabel(@sprintf("P [%s]", punit))
+            pl.title(@sprintf("%s Load, RP: SC=%2.f [%sh], op=%3.f %s", title, sc, punit, (op-1.0)*100.0, prozent))
             pl.legend()
             pl.savefig(joinpath(fig_dir, @sprintf("%s_1_%2.f_%3.f.png", title, sc, (op-1.0)*100.0)))
 
             pl.figure(fig[1]); fig[1] += 1
-            pl.plot(dates, stg.IF,   label="IF")
             pl.plot(dates, Load,   label="Load")
-            pl.title(@sprintf("%s Storage power inflow %2.f %s, %3.f %s", title, sc, punit, (op-1.0)*100.0, prozent))
+            pl.plot(dates, stg.IF,   label="IF")
+            pl.xlabel(@sprintf("P [%s]", punit))
+            pl.title(@sprintf("%s Storage power inflow: SC=%2.f [%sh], op=%3.f %s", title, sc, punit, (op-1.0)*100.0, prozent))
             pl.legend()
             pl.savefig(joinpath(fig_dir, @sprintf("%s_2_%2.f_%3.f.png", title, sc, (op-1.0)*100.0)))
 
             pl.figure(fig[1]); fig[1] += 1
+            pl.plot(dates, Load,   label="Load")
             pl.plot(dates, stg.OF,  label="OF")
-            pl.title(@sprintf("%s Storage power outflow %2.f %s, %3.f %s", title, sc, punit, (op-1.0)*100.0, prozent))
+            pl.xlabel(@sprintf("P [%s]", punit))
+            pl.title(@sprintf("%s Storage power outflow: SC=%2.f [%sh], op=%3.f %s", title, sc, punit, (op-1.0)*100.0, prozent))
             pl.legend()
             pl.savefig(joinpath(fig_dir, @sprintf("%s_3_%2.f_%3.f.png", title, sc, (op-1.0)*100.0)))
 
             pl.figure(fig[1]); fig[1] += 1
+            pl.plot(dates, Load,   label="Load")
             pl.plot(dates, stg.IM, label="IM")
-            pl.title(@sprintf("%s Power import %2.f %s, %3.f %s", title, sc, punit, (op-1.0)*100.0, prozent))
+            pl.xlabel(@sprintf("P [%s]", punit))
+            pl.title(@sprintf("%s Power import: SC=%2.f[%sh], op=%3.f %s", title, sc, punit, (op-1.0)*100.0, prozent))
             pl.legend()
             pl.savefig(joinpath(fig_dir, @sprintf("%s_4_%2.f_%3.f.png", title, sc, (op-1.0)*100.0)))
 
             pl.figure(fig[1]); fig[1] += 1
+            pl.plot(dates, Load,   label="Load")
             pl.plot(dates, stg.CT, label="CT")
-            pl.title(@sprintf("%s Power curtailment %2.f %s, %3.f %s", title, sc, punit, (op-1.0)*100.0, prozent))
+            pl.xlabel(@sprintf("P [%s]", punit))
+            pl.title(@sprintf("%s Power curtailment: SC=%2.f %s, op=%3.f %s", title, sc, punit, (op-1.0)*100.0, prozent))
             pl.legend()
             pl.savefig(joinpath(fig_dir, @sprintf("%s_5_%2.f_%3.f.png", title, sc, (op-1.0)*100.0)))
         end
     end
-end
-
-function time_step(dates)
-    ΔT_ms  = Dates.value(dates[2] - dates[1])
-    ΔT_ms/(3.6e6)
-end
-function nb_years(dates)
-    ΔT_ms_e  = Dates.value(dates[end] - dates[1])
-    Δh_e     = ΔT_ms_e/(3.6e6)
-    Δh_e/(365*24)
-end
-function energy(dates, P)
-    ΔT = time_step(dates)
-    nb_y = nb_years(dates)
-    E = sum(P)*ΔT/nb_y
-    #@infoe ("ΔT =", ΔT, "nb_y =", nb_y, "E =", E)
-    E
 end
 
 function load_data(data_dir, punit)
@@ -401,48 +423,31 @@ end
 """
     load data and compute and plot storage fille levels, original times (15 min)
 """
-function comp_and_plot(data_dir, fig_dir, punit; plot_p = false, plot_all_p = false)
+function comp_and_plot(stc1, stc2, oprod, data_dir, fig_dir, punit; plot_p = false, plot_all_p = false, do_log = false)
 
     dates, Load, RP, RP_de, RP_trend, ΔEL, Load_de, Load_trend, BM = load_data(data_dir, punit)
     RP_de = RP_de + BM
 
-    #@infoe (sum(Load)/8.0,  energy(dates, Load))
-
-    conversion_factor = uconvert("TW", punit)
-
-    stc2  = [14.0, 26.0, 35.0, 45.0] .* conversion_factor
-    oprod = [1.5, 1.2, 1.15, 1.1, 1.05]
-
-    stc2  = [26.0] .* conversion_factor
-    oprod = [1.2]
-
-    # stc1 < stc2
-    stc1 = @. stc2 * 0.1
-    stc2 = @. stc2 - stc1
-
-    (res1, res2, oprod) = compute_storage_fill_level(dates, Load_de, RP_de, punit, stc1, stc2, oprod)
+    (res1, res2, oprod) = compute_storage_fill_level(dates, Load_de, RP_de, punit, stc1, stc2, oprod, do_log = do_log)
     # Vector{(storage_fill, stg1, stg2, sc, op)}
 
     for (stg1, stg2, op) in zip(res1, res2, oprod)
-        dEL = 1.0/energy(dates, Load)
-        IF1  = energy(dates, stg1.IF)*dEL
-        OF1  = energy(dates, stg1.OF)*dEL
-        CT1  = energy(dates, stg1.CT)*dEL
-        IM1  = energy(dates, stg1.IM)*dEL
-        IF2  = energy(dates, stg2.IF)*dEL
-        OF2  = energy(dates, stg2.OF)*dEL
-        CT2  = energy(dates, stg2.CT)*dEL
-        IM2  = energy(dates, stg2.IM)*dEL
-        SF1  = stg1.SF[end]*dEL
-        SF2  = stg2.SF[end]*dEL
+        Load_per_year = energy(dates, Load)
+        IF1  = energy(dates, stg1.IF) / Load_per_year
+        OF1  = energy(dates, stg1.OF) / Load_per_year
+        CT1  = energy(dates, stg1.CT) / Load_per_year
+        IM1  = energy(dates, stg1.IM) / Load_per_year
+        IF2  = energy(dates, stg2.IF) / Load_per_year
+        OF2  = energy(dates, stg2.OF) / Load_per_year
+        CT2  = energy(dates, stg2.CT) / Load_per_year
+        IM2  = energy(dates, stg2.IM) / Load_per_year
+        SF1  = stg1.SF[end] / Load_per_year
+        SF2  = stg2.SF[end] / Load_per_year
 
-        @infoe @sprintf("----------------------")
-        @infoe @sprintf("sc = %10.4e, op = %10.4e", stg1.SC, stg2.SC)
-        @infoe @sprintf("SF1[end] = %8.2e, SF2[end] = %8.2e, Load/y = %8.2e", stg1.SF[end], stg2.SF[end], energy(dates, Load))
-        @infoe @sprintf("IF = %10.4e, OF = %10.4e, IF-OF = %10.4e, SF = %10.4e,  CT = %10.4e, IM = %10.4e", 
-            IF1, -OF1,  IF1+OF1, SF1, CT1, IM1)
-        @infoe @sprintf("IF = %10.4e, OF = %10.4e, IF-OF = %10.4e, SF = %10.4e,  CT = %10.4e, IM = %10.4e", 
-            IF2, -OF2,  IF1+OF2, SF2, CT2, IM2)
+        @infoe @sprintf("==== comp_and_plot ================================================================================")
+        @infoe @sprintf("op = %3.1f,  sc1 = %10.4e, sc2 = %10.4e, SF1[end] = %8.2e, SF2[end] = %8.2e, Load/y = %8.2e", op, stg1.SC, stg2.SC, stg1.SF[end], stg2.SF[end], Load_per_year)
+        @infoe @sprintf("IF1 = %10.4e, OF1 = %10.4e, IF1-OF1 = %10.4e, SF1[end] = %10.4e, CT1 = %10.4e, IM1 = %10.4e", IF1, OF1,  IF1-OF1, SF1, CT1, IM1)
+        @infoe @sprintf("IF2 = %10.4e, OF2 = %10.4e, IF2-OF2 = %10.4e, SF2[end] = %10.4e, CT2 = %10.4e, IM2 = %10.4e", IF2, OF2,  IF1-OF2, SF2, CT2, IM2)
 
     end
 
