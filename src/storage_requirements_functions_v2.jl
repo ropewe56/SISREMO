c778d21c72b6fef6cc270658a2b7a7b438f8425b
wester <rolf.wester@mailbox.org>
2023-04-12 13:07:57 +0200

    v2

--- src/storage_requirements_functions_v2.jl
+++ src/storage_requirements_functions_v2.jl
@@ -113,7 +113,7 @@ function write_power_step_to_log(stg::Storage, out, i, j)
         i, j, stg.SF[i], P, L, stg.I2[i], stg.I3[i], stg.I4[i], stg.I5[i], stg.I6[i], stg.I7[i]))
 end
 
-function power_step(stg::Storage, P, L, i; out = nothing)
+function power_step(stg::Storage, L, P, i; out = nothing)
     ΔSC = stg.SC - stg.SF[i-1]
     if P - L > 0.0
         I2[i] = L
@@ -129,45 +129,6 @@ function power_step(stg::Storage, P, L, i; out = nothing)
     end
 end
 
-
-function charge(stg::Storage, ΔPin, i; out = nothing)
-    local ΔPout
-    ΔSC = stg.SC - stg.SF[i-1]
-    if ΔPin > 0.0
-        if ΔSC > ΔPin
-            stg.IF[i] = ΔPin
-            stg.SF[i] = stg.SF[i-1] + stg.IF[i]*st.ηin
-            ΔPout    = 0.0     # Load is satisfied
-
-            if out !== nothing write_to_log(stg, ΔPin, out, i, 1) end
-        else
-            stg.IF[i] = ΔSC
-            stg.SF[i] = stg.SF[i-1] + stg.IF[i]*st.ηin
-            stg.CT[i] = ΔPin - stg.IF[i] # to be curtailed (or exported)
-            ΔPout     = stg.CT[i] # Load is staisfied, ΔPout remains
-
-            if out !== nothing write_to_log(stg, ΔPin, out, i, 2) end
-        end
-    else
-        if stg.SF[i-1] > -ΔPin   # ΔPin < 0
-            stg.OF[i] = -ΔPin    # stg.OF[i] < 0
-            stg.SF[i] = stg.SF[i-1] - stg.OF[i]
-            ΔPout     = 0.0       # Load is satisfied
-
-            if out !== nothing write_to_log(stg, ΔPin, out, i, 3) end
-        else
-            stg.OF[i] = stg.SF[i-1]
-            stg.SF[i] = stg.SF[i-1] - stg.OF[i]
-            stg.RE[i] = -ΔPin - stg.OF[i] # to be imported
-            ΔPout     = -stg.IM[i]        # missing for Load to be satisfied
-
-            if out !== nothing write_to_log(stg, ΔPin, out, i, 4) end
-        end
-    end
-    ΔPout
-end
-
-
 """
     compute_storage_level(dates, Load, RP, punit, over_production, storage_capacity)
 
@@ -181,48 +142,36 @@ end
     storage_capacity : storage capacity
 """
 function compute_storage_level(dates::Vector{DateTime}, Load::Vector{Float64}, RP::Vector{Float64},
-    stc1::Float64, stc2::Float64, oprod::Float64; do_log = false)
+    st_capacities, oprod::Float64; do_log = false)
 
     # renewable energy over production
     RP = RP .* oprod
-    # surplus or shortfall power production
-    ΔP = (RP - Load)
 
-    out1, out2, out3 = nothing, nothing, nothing
-    if do_log
-        out1 = open(@sprintf("log_%3.1f_1.log", oprod), "w")
-        out2 = open(@sprintf("log_%3.1f_2.log", oprod), "w")
-        out3 = open(@sprintf("log_%3.1f_3.log", oprod), "w")
+    nb_steps  = size(RP, 1)
+    for stc in st_capacities
+        storage = Storage(stc, nb_steps)
+        storage.SF[1] = stc*0.5
+        push!(storages, storage)
     end
 
-    n  = size(RP, 1)
-    stg1 = Storage(stc1, n)
-    stg2 = Storage(stc2, n)
     for i in 2:n
-        ΔPout1 = charge(stg1, ΔP[i],  i, out = out1)
-        ΔPout2 = charge(stg2, ΔPout1, i, out = out2)
-
-        balance1 = stg1.IF[i] - stg1.OF[i] - (stg1.SF[i] - stg1.SF[i-1])
-        balance2 = stg2.IF[i] - stg2.OF[i] - (stg2.SF[i] - stg2.SF[i-1])
-        if abs(balance1) > 1.0e-10 || abs(balance2) > 1.0e-10
-            write(out3, @sprintf("%d  b1 = %9.2e  b2 = %9.2e  P1 = %9.2e  P2 = %9.2e\n", i, balance1, balance2, ΔPout1, ΔPout2))
+        power_step(storages[1], L, P, i, out = out1)
+        for j in 2:length(storages)
+            charge(storages[j], storages[j-1].I7[i], storages[j-1].I5[i], i, out = out2)
         end
     end
 
-    @infoe @sprintf("op = %3.1f, SC1 = %8.2e, SC2 = %8.2e, SF1[end] = %8.2e, SF2[end] = %8.2e", oprod, stg1.SC, stg2.SC, stg1.SF[end], stg2.SF[end])
+    for (j, stg) in enumerate(storages)
+        direct = sum(stg.I2)
+        st_in  = sum(stg.I4)
+        st_out = sum(stg.I6)
+        other  = sum(stg.I7)
+        curt   = sum(stg.I5)
+        @infoe @sprintf("j = %d, op = %3.1f, SC = %8.2e, SF[end] = %8.2e", j, oprod, stg.SC, stg.SF[end])
+        @infoe @sprintf("direct = %8.2e, st_in = %8.2e, st_out = %8.2e, other = %8.2e, curt = %8.2e", direct, st_in, st_out, other, curt)
+    end
 
-    IF1 = sum(stg1.IF)
-    OF1 = sum(stg1.OF)
-    SF1 = stg1.SF[end]
-
-    IF2 = sum(stg2.IF)
-    OF2 = sum(stg2.OF)
-    SF2 = stg2.SF[end]
-
-    @infoe @sprintf("IF1 = %9.3e, OF1 = %9.3e, SF1[end] = %9.3e, B1 = %9.3e", IF1, OF1, SF1,  IF1 - OF1 - SF1)
-    @infoe @sprintf("IF2 = %9.3e, OF2 = %9.3e, SF2[end] = %9.3e, B2 = %9.3e", IF2, OF2, SF2,  IF2 - OF2 - SF2)
-
-    (stg1, stg2)
+    storages
 end
 
 """
